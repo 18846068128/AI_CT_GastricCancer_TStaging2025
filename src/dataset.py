a5ab81ea7507e8ad1531ba3616687f9e0b65c632
18846068128 <2023120530@cmu.edu.cn>
2025-05-21 17:54:49 +0800

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -1,23 +1,25 @@
 import os
 import numpy as np
-import pandas as pd
 from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms
 
 class CTDataset(Dataset):
-    def __init__(self, root_dir, df, transform=None):
+    def __init__(self, root_dir, image_paths, labels, transform=None):
         self.root_dir = root_dir
         self.transform = transform
-        self.image_paths = [os.path.join(root_dir, img) for img in df['image_name']]
-        self.labels = df['label'].values.astype(np.int64)
+        self.image_paths = image_paths
+        self.labels = labels.astype(np.int64)
 
     def __len__(self):
         return len(self.image_paths)
 
     def __getitem__(self, idx):
-        image = Image.open(self.image_paths[idx]).convert('RGB')
+        img_path = self.image_paths[idx]
+        image = Image.open(img_path).convert('RGB')
         label = self.labels[idx]
+        
         if self.transform:
             image = self.transform(image)
-        return image, label, self.image_paths[idx]
+            
+        return image, label, img_path
